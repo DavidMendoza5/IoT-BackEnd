3f3ad4c0320111752fef4088761d0cf9df3de537
David <cruz-azul99@hotmail.com>
2021-10-23 11:57:04 -0500

    New get route created

--- src/routes/temperature.routes.js
+++ src/routes/temperature.routes.js
@@ -5,6 +5,7 @@ const temperatureController = require('../controllers/temperature.controller')
 const api = express.Router()
 
 api.get('/temperature', temperatureController.getTemperature)
+api.get('/temperature/data', temperatureController.getTemperatureFromDb)
 api.post('/temperature', temperatureController.activateRele)
 
 module.exports = api
